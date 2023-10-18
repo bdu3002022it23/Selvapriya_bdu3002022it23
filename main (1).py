82f61e347fa810ef5022a7e167719261efed31fa
Selvapriya_797E5C1FA6A70929CC8493B3EFD02337 <145659118+bdu3002022it23@users.noreply.github.com>
2023-10-18 13:25:50 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,12 @@
+#1.1 Implement a recursive function to calculate the factorial of a given number
+
+def fact_rec (n);
+if n= = 0 or n== 1:
+return 1
+else:
+return n*fact_ res(n-1)
+
+number = int (input("Enter a number:"))
+res = fact_res (number)
+
+print ("factorial of {} is {}.". format(numbers,rus))
